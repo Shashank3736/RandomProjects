df2764163571c11106ba11428875d0f75f0f9ff6
Shreyash Raj <shreyashr267@gmail.com>
2022-09-13 12:08:30 +0530

    Shifted code to main function

--- BitMapWorld/bitmapworld.py
+++ BitMapWorld/bitmapworld.py
@@ -1,19 +1,23 @@
 import sys
 
-bitmap = ''
-with open('BitMapWorld/bitmapworld.txt') as f:
-    bitmap = f.read()
+def main():
+    bitmap = ''
+    with open('BitMapWorld/bitmapworld.txt') as f:
+        bitmap = f.read()
 
-print('BitMapWorld by Shreyash Raj.')
-print('Enter the message to display with the bitmap.')
-message = input('> ')
-if message == '':
-    sys.exit()
+    print('BitMapWorld by Shreyash Raj.')
+    print('Enter the message to display with the bitmap.')
+    message = input('> ')
+    if message == '':
+        sys.exit()
 
-for line in bitmap.splitlines():
-    for i, bit in enumerate(line):
-        if bit == ' ':
-            print(' ', end='')
-        else:
-            print(message[i%len(message)], end='')
-    print()
+    for line in bitmap.splitlines():
+        for i, bit in enumerate(line):
+            if bit == ' ':
+                print(' ', end='')
+            else:
+                print(message[i%len(message)], end='')
+        print()
+
+if __name__ == '__main__':
+    main()
